7a6660dcf7c2b248b3f8546f94fee4d6806eb58e
Cheol Kang <g6lingp@gmail.com>
2019-01-14 01:07:39 +0900

    Feat: Add time

--- run-in-gcloud.py
+++ run-in-gcloud.py
@@ -3,10 +3,13 @@ import multiprocessing
 from multiprocessing.dummy import Pool
 from subprocess import call
 
-import time
+import timeit
 
+# gcloud compute instances start pytorch-1-vm
+# gcloud compute ssh --project pytorch-ml-228505 --zone us-west1-b pytorch-1-vm -- -L 8080:localhost:8080
 
 if __name__ == '__main__':
+    start = timeit.default_timer()
 
     pool = Pool(multiprocessing.cpu_count() - 1)
     commands = []
@@ -19,6 +22,11 @@ if __name__ == '__main__':
     for i, return_code in enumerate(pool.imap(partial(call, shell=True), commands)):
         if return_code != 0:
             print('%d command fail: %d, %s' % (i, return_code, commands[i]))
+
+    
+    stop = timeit.default_timer()
+
+    print('Time: ', stop - start)
 
 
 
