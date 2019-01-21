d94bfeae5a3c36a3ce96de65880241ad5ba1bf0f
Cheol Kang <g6lingp@gmail.com>
2019-01-21 22:57:37 +0900

    gpu

--- src/config.py
+++ src/config.py
@@ -4,8 +4,8 @@ gamma = 0.99
 batch_size = 64
 lr = 0.01
 replay_memory_capacity = 100
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = "cpu"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = "cpu"
 
 sequence_length = 8
 burn_in_length = 4
